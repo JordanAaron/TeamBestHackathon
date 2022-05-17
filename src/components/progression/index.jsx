f8ab0907e35e3a26e773e7471252493948b552cd
Jordan Quartey <jordev14@gmail.com>
2022-05-17 14:06:53 +0100

    initial commit

---
+++ src/components/progression/index.jsx
@@ -0,0 +1,160 @@
+import React, { useEffect, useState } from "react";
+import {
+  Accordion,
+  Dropdown,
+  DropdownButton,
+  Form,
+  Modal,
+  Tab,
+  Button,
+} from "react-bootstrap";
+import Tabs from "react-bootstrap/Tabs";
+
+import "./progression.css";
+import { data } from "../../data";
+
+// import { img } from "../../assets/luna-logo.png";
+
+export const Progression = () => {
+  const [role, setRole] = useState("PD");
+  const [jsonData, setJsonData] = useState(data.pd);
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    if (role === "PD") {
+      setJsonData(data.pd);
+    } else if (role === "PA") {
+      setJsonData(data.pa);
+    } else {
+      setJsonData(data.ux);
+    }
+  }, [role]);
+
+  return (
+    <div>
+      <Modal show={showModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Feedback Input</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <textarea className="feedback-entry" placeholder="Enter feedback" />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Close
+          </Button>
+          <Button className="main-btn" onClick={() => setShowModal(false)}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <div className="header">
+        <img src={require("../../assets/luna-logo.png")} alt="luna logo" />
+        <h1>Level 2</h1>
+        <div className="role-filter">
+          <DropdownButton title={role} className="main-btn">
+            <Dropdown.Item
+              key={0}
+              onClick={() => {
+                setRole("PD");
+              }}
+            >
+              PD
+            </Dropdown.Item>
+            <Dropdown.Item
+              key={1}
+              onClick={() => {
+                setRole("PA");
+              }}
+            >
+              PA
+            </Dropdown.Item>
+            <Dropdown.Item
+              key={2}
+              onClick={() => {
+                setRole("UX");
+              }}
+            >
+              UX
+            </Dropdown.Item>
+          </DropdownButton>
+        </div>
+      </div>
+      <div className="background">
+        {/* <hr /> */}
+        <div className="summary-section">
+          <h3>Summary</h3>
+          <p>{jsonData.summary}</p>
+        </div>
+        <div className="target-section">
+          <h3>Targets</h3>
+          <Tabs defaultActiveKey="Behaviour" className="mb-3 tabtab">
+            {jsonData.skills.map((item) => {
+              let target = item.body;
+              return (
+                <Tab key={item.title} eventKey={item.title} title={item.title}>
+                  <Form>
+                    {target.ex1 ? (
+                      <div className="target">
+                        <Form.Check />
+                        <Accordion>
+                          <Accordion.Header>{target.ex1.body}</Accordion.Header>
+                          <Accordion.Body>
+                            <hr />
+                            {target.ex1.feedback}
+                            <div
+                              className="add-button"
+                              onClick={() => setShowModal(true)}
+                            >
+                              +
+                            </div>
+                          </Accordion.Body>
+                        </Accordion>
+                      </div>
+                    ) : null}
+                    {target.ex2 ? (
+                      <div className="target">
+                        <Form.Check />
+                        <Accordion>
+                          <Accordion.Header>{target.ex2.body}</Accordion.Header>
+                          <Accordion.Body>
+                            <hr />
+                            {target.ex2.feedback}
+                            <div
+                              className="add-button"
+                              onClick={() => setShowModal(true)}
+                            >
+                              +
+                            </div>
+                          </Accordion.Body>
+                        </Accordion>
+                      </div>
+                    ) : null}
+                    {target.ex3 ? (
+                      <div className="target">
+                        <Form.Check />
+                        <Accordion>
+                          <Accordion.Header>{target.ex3.body}</Accordion.Header>
+                          <Accordion.Body>
+                            <hr />
+                            {target.ex3.feedback}
+                            <div
+                              className="add-button"
+                              onClick={() => setShowModal(true)}
+                            >
+                              +
+                            </div>
+                          </Accordion.Body>
+                        </Accordion>
+                      </div>
+                    ) : null}
+                  </Form>
+                </Tab>
+              );
+            })}
+          </Tabs>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/data.js
@@ -0,0 +1,192 @@
+var data = {
+  pd: {
+    summary:
+      "Associates level 2 are working under relatively close supervision of their manager but are expected to start to plan delivery of their own  activities. They understand and can explain the basics of both PA and PD Product Creation skills (coding principles, UX, requirement gathering, analysis and design patterns) and through delivery of high quality work are building a good level of trust & respect within the client and their own squads. They are also proactively identifying opportunities to build the required skills that align with the PA/PD career path.",
+    skills: [
+      {
+        title: "Behaviour",
+        body: {
+          ex1: {
+            title: "Communicate with empathy",
+            body: "You're aware of your personal impact and communication style .You actively contribute in team discussions. You get your point across clearly, accurately and professionally. You keep people up to date.",
+            feedback: "Feedback example 1: ducks",
+          },
+          ex2: {
+            title: "Delighting the client",
+            body: "You understand your own role in delivering value for the customer. You respond positively to what is expected and deliver to expectations. You set appropriate standards for your own performance and monitor progress against plans.",
+            feedback: "Feedback example 1: neck",
+          },
+          ex3: {
+            title: "Developing others",
+            body: "You are supportive of others’ development. You are keen to develop and practice your own coaching skills.",
+            feedback: "Feedback example 1: pluto",
+          },
+        },
+      },
+      {
+        title: "Delivery",
+        body: {
+          ex1: {
+            title: "Agile and Lean practices",
+            body: "You understand the importance of having an agile mindset and an awareness of key tools and techniques",
+            feedback: "Feedback example 1: what",
+          },
+        },
+      },
+      {
+        title: "Engineering",
+        body: {
+          ex1: {
+            title: "Code quality",
+            body: "You understand key concepts such as 'Don't Repeat Yourself', and the importance of naming things. ",
+            feedback: "Feedback example 1: sorry",
+          },
+          ex2: {
+            title: "Data structure and algorithms",
+            body: "You can recognise data structure and understand the differences between them.",
+            feedback: "Feedback example 1: one",
+          },
+          ex3: {
+            title: "Object oriented and functional programming",
+            body: "You understand the different between OOP and FP.",
+            feedback: "Feedback example 1: sandy",
+          },
+        },
+      },
+    ],
+  },
+  pa: {
+    summary:
+      "Level 3 (PA or PD) can work in autonomy on most parts of their project. They have a good understanding of what good looks like and know how to stretch their own limits. They express ideas clearly, concisely and professionally and do not hesitate to ask for help when needed. They are respected and trusted by other team members to provide guidance as needed on both client and internal work. They proactively acquire and develop skills that are relevant to their areas of interest and desired specialism(s).",
+    skills: [
+      {
+        title: "Behaviour",
+        body: {
+          ex1: {
+            title: "Communicate with empathy",
+            body: "Level 3 PA example where we can grill fish on a barbecue of pineapples",
+            feedback: "Feedback example 1: what am i doing",
+          },
+          ex2: {
+            title: "Delighting the client",
+            body: "Eat bread",
+            feedback: "Feedback example 1: mm hmm",
+          },
+          ex3: {
+            title: "Developing others",
+            body: "You are keen to develop and practice your own coaching skills and drink milk.",
+            feedback: "Feedback example 1: learned",
+          },
+        },
+      },
+      {
+        title: "Delivery",
+        body: {
+          ex1: {
+            title: "Agile and Lean practices",
+            body: "You identify the appropriate delivery methods. You encourage an experimentation mindset and adapt.",
+            feedback: "Feedback example 1: working on",
+          },
+          ex2: {
+            title: "Budget management",
+            body: "You know how to balance cost versus value, manage a budget and escalate issues.",
+            feedback: "Feedback example 1: let it go",
+          },
+          ex3: {
+            title: "Delivery governance",
+            body: "You understand Programme and Project Governance structures. You can input into team structures.",
+            feedback: "Feedback example 1: information overload",
+          },
+        },
+      },
+      {
+        title: "Product",
+        body: {
+          ex1: {
+            title: "Product Analysis",
+            body: "You can contribute  and understand the importance of evidence-based design or analysis",
+            feedback: "Feedback example 1: visibly easier",
+          },
+          ex2: {
+            title: "Product Discovery",
+            body: "You have limited experience and need guidance",
+            feedback: "Feedback example 1: afternoon to a panel of judges",
+          },
+          ex3: {
+            title: "Product life-cycle management",
+            body: "You understand the tools, terms and concepts used to deliver a product.",
+            feedback: "Feedback example 1: buenas tardes",
+          },
+        },
+      },
+    ],
+  },
+  ux: {
+    summary:
+      "Level 4 are senior PA or PD, and experts in their specialism. They can deliver their work in complete autonomy, while providing clear and regular communications to team members and client/project stakeholders. They articulate themselves well in area of subject matter expertise and are respected and trusted by other team members to provide sound judgment and direction as an expert in their subjects. They are confident and comfortable operating in uncertainty helping to shape and deliver high quality work. They play a key role in creating and maintaining a positive project team environment.",
+    skills: [
+      {
+        title: "Behaviour",
+        body: {
+          ex1: {
+            title: "Communicate with empathy",
+            body: "You adapt your personal style, communication method and level of technical detail to suit the situation / audience. You listen attentively and respond appropriately to non verbal cues from others. You expresse disagreement with sensitivity and tact. You're empathetic towards others' issues and concerns.",
+            feedback: "Feedback example 1: gaurav gupta",
+          },
+          ex2: {
+            title: "Delighting the client",
+            body: "You take personal accountability for results. You encourage the team to cultivate customer / stakeholder relationships across the business. You keep sight of long term objectives in daily work. You actively drive collaboration and shared accountabilities with multiple stakeholders. You show resilience when faced with setbacks.",
+            feedback: "Feedback example 1: luckily",
+          },
+          ex3: {
+            title: "Developing others",
+            body: "You play an active role in helping others develop and grow their potential. You give honest, supportive and developmental feedback. You act as a buddy, coach or mentor. You tie your personal development to company / work objectives.",
+            feedback: "Feedback example 1: queen",
+          },
+        },
+      },
+      {
+        title: "Delivery",
+        body: {
+          ex1: {
+            title: "Agile and Lean practices",
+            body: "You can advise colleagues on how and why agile methods are used and provide a clear, open and transparent framework in which teams can deliver.",
+            feedback: "Feedback example 1: cheese",
+          },
+          ex2: {
+            title: "Budget management",
+            body: "You understand the live state of the budget for the project",
+            feedback: "Feedback example 1: vegan cheese",
+          },
+          ex3: {
+            title: "Delivery governance",
+            body: "You are aware of Programme and Project Governance structures and their purpose in our engagements.",
+            feedback: "Feedback example 1: curry goat",
+          },
+        },
+      },
+      {
+        title: "Experience Design",
+        body: {
+          ex1: {
+            title: "Content design",
+            body: "You take responsibility for creating and reviewing content for user journeys. presenting the right information in the right format. You drive ideation and can work with and on design systems and content guides.",
+            feedback: "Feedback example 1: Unlucky gurneet",
+          },
+          ex2: {
+            title: "Information architecture",
+            body: "You employ research methods to specifically understand user relationships to information (e.g card sorting). You can uncover and describe the user’s mental model and problem space.",
+            feedback: "Feedback example 1: Better luck next time Helen",
+          },
+          ex3: {
+            title: "Interaction design",
+            body: "You're able to identify user issues, interpret research and design based on evidence. You can design for different types of interface. You're comfortable working with and on design systems.",
+            feedback: "Feedback example 1: Unfortunate scenes Fahim",
+          },
+        },
+      },
+    ],
+  },
+};
+
+export { data };
